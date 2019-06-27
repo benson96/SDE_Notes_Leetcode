695c495eeb5976cafa8b354b4a364ca9add9aead
Benson96 <jjiang17@ucsc.edu>
2019-06-27 16:05:53 -0700

    not finished

---
+++ Sample_Code/BinarySearch/KClosestPosition.java
@@ -0,0 +1,45 @@
+/*
+
+
+
+*/
+
+
+public class KClosestPosition {
+
+	public int [] kCloestPosition(int[] array, int target){
+
+		//corner case:
+
+		if(array == null || array.length == 0){
+			return -1;
+		}
+
+
+		int left = 0; 
+		int right = array.length - 1;
+
+
+
+		while (left + 1 < right){
+
+			int mid = left +  (right - left) /2 ;
+
+			if (array[mid] =)
+
+
+
+
+
+		}
+
+
+
+
+
+	}
+
+
+
+
+}
