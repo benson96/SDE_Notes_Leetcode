43667cc4a773390c04de7cc1848a01acdf26b528
Benson96 <jjiang17@ucsc.edu>
2019-07-09 16:17:40 -0700

    update

--- note/July_8.java
+++ note/July_8.java
@@ -15,7 +15,102 @@ July 8:
 	<4> LinkedList比ArrayList更占内存，因为LinkedList的节点除了存储数据，
 		还存储了两个引用，一个指向前一个元素，一个指向后一个元素。
 
-3. Interface vs Implementation vs class 
+3. Stack and Queue:
 
 
-	<1> 
+	Stack:
+
+	Linear structure: last in first out  
+
+	只在 表尾 进行删除和操作
+
+	特殊的线性表，
+	但在操作上有特殊的要求和限制
+	后进先出
+
+	对于 stack, 表尾 称为 栈的栈顶 (TOP)
+				表头 称为 栈底 (BOTTOM)
+
+Modifier and Type		Method and Description
+
+boolean					empty()
+						Tests if this stack is empty.
+
+E						peek()
+						Looks at the object at the top of this stack without removing it from the stack.
+
+E						pop()
+						Removes the object at the top of this stack and returns that object as the value of this function.
+
+E						push(E item)
+						Pushes an item onto the top of this stack.
+
+int						search(Object o)
+						Returns the 1-based position where an object is on this stack.
+
+
+Queue
+
+First In First Out:
+
+Modifier and Type			Method and Description
+boolean						add(E e)
+							Inserts the specified element into this queue if it is possible to do so immediately without violating capacity restrictions, returning true upon success and throwing an IllegalStateException if no space is currently available.
+
+E							element()
+							Retrieves, but does not remove, the head of this queue.
+
+boolean						offer(E e)
+							Inserts the specified element into this queue if it is possible to do so immediately without violating capacity restrictions.
+
+E							peek()
+							Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
+
+E							poll()
+							Retrieves and removes the head of this queue, or returns null if this queue is empty.
+
+E							remove()
+							Retrieves and removes the head of this queue.
+
+
+4. Deque
+
+双向队列
+
+Methods of Deque:
+
+add(element): Adds an element to the tail.
+
+addFirst(element): Adds an element to the head.
+
+addLast(element): Adds an element to the tail.
+
+offer(element): Adds an element to the tail and returns a boolean to explain if the insertion was successful.
+
+offerFirst(element): Adds an element to the head and returns a boolean to explain if the insertion was successful.
+
+offerLast(element): Adds an element to the tail and returns a boolean to explain if the insertion was successful.
+
+iterator(): Returna an iterator for this deque.
+
+descendingIterator(): Returns an iterator that has the reverse order for this deque.
+
+push(element): Adds an element to the head.
+
+pop(element): Removes an element from the head and returns it.
+
+removeFirst(): Removes the element at the head.
+
+removeLast(): Removes the element at the tail.
+
+poll(): Retrieves and removes the head of the queue represented by this deque (in other words, the first element of this deque), or returns null if this deque is empty.
+
+pollFirst(): Retrieves and removes the first element of this deque, or returns null if this deque is empty.
+
+pollLast(): Retrieves and removes the last element of this deque, or returns null if this deque is empty.
+
+peek(): Retrieves, but does not remove, the head of the queue represented by this deque (in other words, the first element of this deque), or returns null if this deque is empty.
+
+peekFirst(): Retrieves, but does not remove, the first element of this deque, or returns null if this deque is empty.
+
+peekLast(): Retrieves, but does not remove, the last element of this deque, or returns null if this deque is empty.
